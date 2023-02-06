88b716904cd77a1682d1401a531bc8f36f451315
Arunjenson <75872046+Arunjenson@users.noreply.github.com>
2023-02-06 22:18:00 +0530

    signup initial commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Submit the signup information to your API for processing
+    };
+
     return (
-        <div>Signup</div>
-    )
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text"
+                    id="firstName"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+            </div>
+
+            <div>
+                <label htmlFor='email'>Email:</label>
+                <input
+                    type='email'
+                    id='email'
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+            </div >
+            <div>
+                <label htmlFor='password'>Password:</label>
+                <input
+                    type='password'
+                    id='password'
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+            </div >
+            <button type='submit'>Sign Up</button>
+        </form >
+    );
+
 }
 
 export default Signup
